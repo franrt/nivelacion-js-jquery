fd98a7b8709d62027cb2d1d755150eae5889baec
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-07-10 11:18:21 -0400

    En index.html aparece menu, al hacer click conecta con recipe.html; en recipe.html aparece flecha, al hacer click regresa a index.html

--- js/script-index.js
+++ js/script-index.js
@@ -1,45 +1,53 @@
-$(document).ready( function(){
+$(document).ready(function() {
 
-	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
-	renderHighlightedRecipes(recipesArray);
+    //esconder menú flecha
+
+    $(".nav-item").on(function(event) {
+        $("#arrow").hide();
+        $("#menu").show();
+    });
+
+
+
+
+    //La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
+    renderHighlightedRecipes(recipesArray);
 
 });
 
 
 /*
-* Función que se encarga de pintar TODAS las recetas que tengan 
-* marcado el atributo "highlighted" como TRUE
-*/
+ * Función que se encarga de pintar TODAS las recetas que tengan 
+ * marcado el atributo "highlighted" como TRUE
+ */
 function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
+    console.log('Recipes: ', recipesArray);
 }
 
 /*
-* Función que se encarga de pintar UNA recetas que tenga 
-* marcado el atributo "highlighted" como TRUE
-* Aqui se tiene que crear el HTML que esta en el 
-* archivo "templates/templates-recipe.html"
-*/
+ * Función que se encarga de pintar UNA recetas que tenga 
+ * marcado el atributo "highlighted" como TRUE
+ * Aqui se tiene que crear el HTML que esta en el 
+ * archivo "templates/templates-recipe.html"
+ */
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
+    console.log('Voy a pintar la receta: ', recipe);
 }
 
 
 
 /*
-* Función que se encarga de pintar todas las actividades
-*/
+ * Función que se encarga de pintar todas las actividades
+ */
 function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
+    console.log('Activities: ', activitiesArray);
 }
 
 /*
-* Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el 
-* archivo "templates/templates-activity.html"
-*/
+ * Función que se encarga de pintar una actividad
+ * Aqui se tiene que crear el HTML que esta en el 
+ * archivo "templates/templates-activity.html"
+ */
 function renderActivity(recipe) {
-	
+
 }
-
-
