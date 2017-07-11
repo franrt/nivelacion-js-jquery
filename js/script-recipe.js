3cd5b4ce05bda76e483f43c1b6c5c3fb13adf757
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-07-10 20:37:34 -0400

    ETAPA 8 (anterior era la 7): funciones en jQuery con las que se quita y agrega la clase .make según el botón al que se le da click

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
     console.log("HTML de Recipe listo");
 
+    //------ Etapa 8
+    //funcion para .js-show-recipe
 
+    $(".js-show-recipe").click(function() {
+        $(".page").removeClass("make");
+    })
+
+    //funcion para .js-show-make
+    $(".js-show-make").click(function() {
+        $(".page").addClass("make");
+    })
 
 
 
